47f478f8687dc98b6708f48497c02ceb428c8007
Emre-Kul <emrekul35@gmail.com>
2018-04-05 16:43:01 +0300

    main.jsx created

---
+++ src/Main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const APP_ELEMENT = document.getElementById("root");
+
+ReactDOM.render(
+    <div>
+        Hello React!
+    </div>,
+    APP_ELEMENT);
